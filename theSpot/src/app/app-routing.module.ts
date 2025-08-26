735851a19d3eca7a768deff756d0ded3616f4ab5
Cole Lenting <124871095+coleLenting@users.noreply.github.com>
2025-08-26 23:45:25 +0200

    basic routing

--- theSpot/src/app/app-routing.module.ts
+++ theSpot/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { CartComponent } from './cart/cart.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'items', component: ItemListComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'admin', component: AdminPanelComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- theSpot/src/app/app.module.ts
+++ theSpot/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { CartComponent } from './cart/cart.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ItemListComponent,
+    CartComponent,
+    AdminPanelComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
